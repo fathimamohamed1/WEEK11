e98fdb1163f9fb1330c30ab14c52b4e6c2a5962c
hey <blabla>
2022-05-09 13:19:51 -0500

    Initial commit

---
+++ first_express_api/hello-vue/src/services/HelloAPI.js
@@ -0,0 +1,13 @@
+
+import axios from 'axios'
+import {response} from "express";
+
+let base ='api'
+
+export  default {
+    getHelloMessage(){
+        return axios.get(base).then(response=>{
+            return response.data
+        })
+    }
+}
